39dea0efa5ad915f007bc82683b80cbb9260d370
KyoheiAbc <autumn8november@gmail.com>
2026-08-17 22:06:30 +0900

    Refactor cube and group creation logic for improved readability and maintainability

--- LEGO_Minifigure.js
+++ LEGO_Minifigure.js
@@ -1,57 +1,44 @@
-function remove() {
-	[...Outliner.root].forEach(element => element.remove());
+[...Outliner.root].forEach(element => element.remove());
+
+function addVec(a, b) {
+	const result = [];
+	for (let i = 0; i < a.length; i++) {
+		result.push(a[i] + b[i]);
+	}
+	return result;
 }
 
-const newCube = (name, from, size) => new Cube({
-	name,
-	from,
-	to: from.map((v, i) => v + size[i]),
-	origin: from.map((v, i) => v + size[i] / 2)
-}).init();
+function createCube(name, from, size) {
+	const halfSize = [size[0] / 2, size[1] / 2, size[2] / 2];
+	return new Cube({
+		name,
+		from,
+		to: addVec(from, size),
+		origin: addVec(from, halfSize)
+	}).init();
+}
 
+function createGroup(cube, extraCubes = [], rot = {}, isPivotTop = false) {
+	const origin = [...cube.origin];
+	if (isPivotTop) origin[1] = cube.to[1] - (cube.to[0] - cube.from[0]) / 2;
 
-function newGroup(cube) {
-	const group = new Group({ name: cube.name }).init();
+	const group = new Group({
+		name: cube.name,
+		origin,
+		rotation: [rot.x || 0, rot.y || 0, rot.z || 0]
+	}).init();
+
 	cube.addTo(group);
-	group.origin[0] = cube.origin[0];
-	group.origin[1] = cube.origin[1];
-	group.origin[2] = cube.origin[2];
-	return group;
+	for (const extra of extraCubes) {
+		extra.addTo(group);
+	}
 }
 
-remove()
-
-
-const rightLeg = newCube("RightLeg", [0, 0, -3], [6, 12, 6])
-const rightLegGroup = newGroup(rightLeg)
-rightLegGroup.origin[1] = rightLeg.to[1] - (rightLeg.to[0] - rightLeg.from[0]) / 2
-
-const leftLeg = newCube("LeftLeg", [-6, 0, -3], [6, 12, 6])
-const leftLegGroup = newGroup(leftLeg);
-leftLegGroup.origin[1] = leftLeg.to[1] - (leftLeg.to[0] - leftLeg.from[0]) / 2
-
-newGroup(newCube("Body", [-6, 12, -3], [12, 12, 6]))
-
-newCube("Neck", [-2, 24, -2], [4, 10, 4]).addTo(newGroup(newCube("Head", [-4, 25, -4], [8, 8, 8])))
-
-const rightHand = newCube("RightHand", [6 - 1.5, 12 - 1, -2], [4, 4, 4])
-const rightArm = newCube("RightArm", [6.5 - 1.5, 16 - 1, -1.5], [3, 8, 3])
-
-const leftHand = newCube("LeftHand", [-10 + 1.5, 12 - 1, -2], [4, 4, 4])
-const leftArm = newCube("LeftArm", [-9.5 + 1.5, 16 - 1, -1.5], [3, 8, 3])
-
-const rightArmGroup = newGroup(rightArm);
-rightHand.addTo(rightArmGroup);
-rightArmGroup.origin[1] = rightArm.to[1] - (rightArm.to[0] - rightArm.from[0]) / 2
-
-const leftArmGroup = newGroup(leftArm);
-leftHand.addTo(leftArmGroup);
-leftArmGroup.origin[1] = leftArm.to[1] - (leftArm.to[0] - leftArm.from[0]) / 2
-
-rightArmGroup.rotation[2] = 15
-leftArmGroup.rotation[2] = -15
-
-rightLegGroup.rotation[1] = -1
-leftLegGroup.rotation[1] = 1
+createGroup(createCube("RightLeg", [0, 0, -3], [6, 12, 6]), [], { y: -1 }, true);
+createGroup(createCube("LeftLeg", [-6, 0, -3], [6, 12, 6]), [], { y: 1 }, true);
+createGroup(createCube("Body", [-6, 12, -3], [12, 12, 6]));
+createGroup(createCube("Head", [-4, 25, -4], [8, 8, 8]), [createCube("Neck", [-2, 24, -2], [4, 10, 4])]);
+createGroup(createCube("RightArm", [5, 15, -1.5], [3, 8, 3]), [createCube("RightHand", [4.5, 11, -2], [4, 4, 4])], { z: 15 }, true);
+createGroup(createCube("LeftArm", [-8, 15, -1.5], [3, 8, 3]), [createCube("LeftHand", [-8.5, 11, -2], [4, 4, 4])], { z: -15 }, true);
 
 Canvas.updateAll();
