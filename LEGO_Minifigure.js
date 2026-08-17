72e093ebbe770bb5d1bda3138cda976169c45412
KyoheiAbc <autumn8november@gmail.com>
2026-08-17 15:35:52 +0900

    Refactor cube creation and removal logic; replace clearAll function with remove

--- LEGO_Minifigure.js
+++ LEGO_Minifigure.js
@@ -1,60 +1,39 @@
-function clearAll() {
+function remove() {
 	[...Outliner.root].forEach(element => element.remove());
 }
 
-function newCube(name, [x, y, z]) {
-	const halfX = x / 2;
-	const halfY = y / 2;
-	const halfZ = z / 2;
-
-	return new Cube({
-		name,
-		from: [-halfX, -halfY, -halfZ],
-		to: [halfX, halfY, halfZ],
-		origin: [0, 0, 0]
-	}).init();
-}
-
-const addOffset = (coords, [dx, dy, dz]) => [
-	coords[0] + dx,
-	coords[1] + dy,
-	coords[2] + dz
-];
-function moveCube(cube, offset) {
-	cube.from = addOffset(cube.from, offset);
-	cube.to = addOffset(cube.to, offset);
-	cube.origin = addOffset(cube.origin, offset);
-
-	Canvas.updateAllPositions();
-}
+const newCube = (name, from, size) => new Cube({
+	name,
+	from,
+	to: from.map((v, i) => v + size[i]),
+	origin: from.map((v, i) => v + size[i] / 2)
+}).init();
 
 
-function assertArrayEqual(actual, expected, message) {
-	const isPass = actual.length === expected.length &&
-		actual.every((val, i) => Math.abs(val - expected[i]) < 1e-6);
-	console.assert(isPass, `${message} | 期待値: [${expected}] 実際: [${actual}]`);
-}
-
-clearAll();
-
-
-
-clearAll();
+remove()
+const cube = newCube("TestCube", [1, 2, 3], [4, 5, 6]);
+const eq = (a, b) => a.every((v, i) => v === b[i]);
+console.assert(cube.name === "TestCube", "名前が一致しません");
+console.assert(eq(cube.from, [1, 2, 3]), `from が不正: ${cube.from}`);
+console.assert(eq(cube.to, [5, 7, 9]), `to が不正: ${cube.to}`);
+console.assert(eq(cube.origin, [3, 4.5, 6]), `origin が不正: ${cube.origin}`);
+remove()
 
 
-// const testCube = newCube('TestCube', [1, 2, 3]);
+const rightLeg = newCube("RightLeg", [0, 0, -3], [6, 12, 6])
+rightLeg.origin[1] = rightLeg.to[1] - (rightLeg.to[0] - rightLeg.from[0]) / 2
+const body = newCube("Body", [-6, 12, -3], [12, 12, 6])
+const neck = newCube("Neck", [-2, 24, -2], [4, 10, 4])
+const head = newCube("Head", [-4, 25, -4], [8, 8, 8])
 
-// console.assert(testCube.name === 'TestCube', '名前が正しく設定されていません');
-// assertArrayEqual(testCube.from, [-0.5, -1, -1.5], 'from の初期値が不正です');
-// assertArrayEqual(testCube.to, [0.5, 1, 1.5], 'to の初期値が不正です');
-// assertArrayEqual(testCube.origin, [0, 0, 0], 'origin の初期値が不正です');
+const rightHand = newCube("RightHand", [6 - 0.5 - 1, 12 - 1, -2], [4, 4, 4])
+const rightArm = newCube("RightArm", [6.5 - 0.5 - 1, 16 - 1, -1.5], [3, 8, 3])
 
-// moveCube(testCube, [10, -5, 2]);
-// assertArrayEqual(testCube.from, [9.5, -6, 0.5], '移動後の from が不正です');
-// assertArrayEqual(testCube.to, [10.5, -4, 3.5], '移動後の to が不正です');
-// assertArrayEqual(testCube.origin, [10, -5, 2], '移動後の origin が不正です');
+const rightArmGroup = new Group({ name: "RightArm" }).init();
+rightArm.addTo(rightArmGroup);
+rightHand.addTo(rightArmGroup);
+rightArmGroup.origin[0] = rightArm.origin[0]
+rightArmGroup.origin[1] = rightArm.to[1] - (rightArm.to[0] - rightArm.from[0]) / 2
+rightArmGroup.origin[2] = rightArm.origin[2]
 
-// clearAll();
-// console.assert(Outliner.root.length === 0, 'clearAll 後に要素が残っています');
-
-// console.log('すべてのテストを通過しました！');
+Canvas.updateAll();
