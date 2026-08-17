9c17925923f6724a13d8ff8d3247b8ad31531b72
KyoheiAbc <autumn8november@gmail.com>
2026-08-17 21:50:09 +0900

    Refactor cube grouping logic and remove test assertions

--- LEGO_Minifigure.js
+++ LEGO_Minifigure.js
@@ -10,30 +10,48 @@ const newCube = (name, from, size) => new Cube({
 }).init();
 
 
-remove()
-const cube = newCube("TestCube", [1, 2, 3], [4, 5, 6]);
-const eq = (a, b) => a.every((v, i) => v === b[i]);
-console.assert(cube.name === "TestCube", "名前が一致しません");
-console.assert(eq(cube.from, [1, 2, 3]), `from が不正: ${cube.from}`);
-console.assert(eq(cube.to, [5, 7, 9]), `to が不正: ${cube.to}`);
-console.assert(eq(cube.origin, [3, 4.5, 6]), `origin が不正: ${cube.origin}`);
+function newGroup(cube) {
+	const group = new Group({ name: cube.name }).init();
+	cube.addTo(group);
+	group.origin[0] = cube.origin[0];
+	group.origin[1] = cube.origin[1];
+	group.origin[2] = cube.origin[2];
+	return group;
+}
+
 remove()
 
 
 const rightLeg = newCube("RightLeg", [0, 0, -3], [6, 12, 6])
-rightLeg.origin[1] = rightLeg.to[1] - (rightLeg.to[0] - rightLeg.from[0]) / 2
-const body = newCube("Body", [-6, 12, -3], [12, 12, 6])
-const neck = newCube("Neck", [-2, 24, -2], [4, 10, 4])
-const head = newCube("Head", [-4, 25, -4], [8, 8, 8])
+const rightLegGroup = newGroup(rightLeg)
+rightLegGroup.origin[1] = rightLeg.to[1] - (rightLeg.to[0] - rightLeg.from[0]) / 2
 
-const rightHand = newCube("RightHand", [6 - 0.5 - 1, 12 - 1, -2], [4, 4, 4])
-const rightArm = newCube("RightArm", [6.5 - 0.5 - 1, 16 - 1, -1.5], [3, 8, 3])
+const leftLeg = newCube("LeftLeg", [-6, 0, -3], [6, 12, 6])
+const leftLegGroup = newGroup(leftLeg);
+leftLegGroup.origin[1] = leftLeg.to[1] - (leftLeg.to[0] - leftLeg.from[0]) / 2
 
-const rightArmGroup = new Group({ name: "RightArm" }).init();
-rightArm.addTo(rightArmGroup);
+newGroup(newCube("Body", [-6, 12, -3], [12, 12, 6]))
+
+newCube("Neck", [-2, 24, -2], [4, 10, 4]).addTo(newGroup(newCube("Head", [-4, 25, -4], [8, 8, 8])))
+
+const rightHand = newCube("RightHand", [6 - 1.5, 12 - 1, -2], [4, 4, 4])
+const rightArm = newCube("RightArm", [6.5 - 1.5, 16 - 1, -1.5], [3, 8, 3])
+
+const leftHand = newCube("LeftHand", [-10 + 1.5, 12 - 1, -2], [4, 4, 4])
+const leftArm = newCube("LeftArm", [-9.5 + 1.5, 16 - 1, -1.5], [3, 8, 3])
+
+const rightArmGroup = newGroup(rightArm);
 rightHand.addTo(rightArmGroup);
-rightArmGroup.origin[0] = rightArm.origin[0]
 rightArmGroup.origin[1] = rightArm.to[1] - (rightArm.to[0] - rightArm.from[0]) / 2
-rightArmGroup.origin[2] = rightArm.origin[2]
+
+const leftArmGroup = newGroup(leftArm);
+leftHand.addTo(leftArmGroup);
+leftArmGroup.origin[1] = leftArm.to[1] - (leftArm.to[0] - leftArm.from[0]) / 2
+
+rightArmGroup.rotation[2] = 15
+leftArmGroup.rotation[2] = -15
+
+rightLegGroup.rotation[1] = -1
+leftLegGroup.rotation[1] = 1
 
 Canvas.updateAll();
