ef87bcda10d64ca281d7f287276d9e046f8de522
KyoheiAbc <autumn8november@gmail.com>
2026-08-18 14:13:04 +0900

    Add size function and verification for cube properties

--- LEGO_Minifigure.js
+++ LEGO_Minifigure.js
@@ -47,12 +47,6 @@ leftArm.rotation[2] = -15
 Canvas.updateAll();
 
 
-function size(cube) {
-	return [cube.to[0] - cube.from[0], cube.to[1] - cube.from[1], cube.to[2] - cube.from[2]];
-}
-
-
-
 console.assert(Group.all[0].name == "RightLeg");
 console.assert(Group.all[0].origin[0] == 3);
 console.assert(Group.all[0].origin[1] == 9);
@@ -107,6 +101,45 @@ console.assert(Group.all[7].children[0].name == Group.all[7].name);
 Group.all.forEach(group => {
 	console.assert(group.origin[2] == 0)
 });
+
+function size(cube) {
+	return [cube.to[0] - cube.from[0], cube.to[1] - cube.from[1], cube.to[2] - cube.from[2]];
+}
+
+function verifyCubeProperties(index, expectedName, expectedSize, expectedOrigin) {
+	const cube = Cube.all[index];
+
+	if (!cube) {
+		console.error(`[Verify Error] Index ${index} に Cube が存在しません。`);
+		return false;
+	}
+
+	const isNameMatched = cube.name === expectedName;
+	const isSizeMatched = JSON.stringify(size(cube)) === JSON.stringify(expectedSize);
+	const isOriginMatched = JSON.stringify(cube.origin) === JSON.stringify(expectedOrigin);
+
+	const isAllMatched = isNameMatched && isSizeMatched && isOriginMatched;
+
+	if (!isAllMatched) {
+		console.warn(`[Verify Failed] Cube[${index}]`, {
+			expected: { name: expectedName, size: expectedSize, origin: expectedOrigin },
+			actual: { name: cube.name, size: size(cube), origin: cube.origin }
+		});
+	}
+
+	return isAllMatched;
+}
+
+verifyCubeProperties(0, "RightLeg", [6, 12, 6], [3, 6, 0]);
+verifyCubeProperties(1, "LeftLeg", [6, 12, 6], [-3, 6, 0]);
+verifyCubeProperties(2, "Body", [12, 12, 6], [0, 18, 0]);
+verifyCubeProperties(3, "Neck", [4, 10, 4], [0, 29, 0]);
+verifyCubeProperties(4, "Head", [8, 8, 8], [0, 29, 0]);
+verifyCubeProperties(5, "RightArm", [3, 8, 3], [6 + 1.5 - 1, 20 - 1, 0]);
+verifyCubeProperties(6, "RightHand", [4, 4, 4], [6 + 1.5 - 1, 14 - 1, 0]);
+verifyCubeProperties(7, "LeftArm", [3, 8, 3], [-6 - 1.5 + 1, 20 - 1, 0]);
+verifyCubeProperties(8, "LeftHand", [4, 4, 4], [-6 - 1.5 + 1, 14 - 1, 0]);
+
 
 Cube.all.forEach(cube => {
 	console.assert(cube.origin[2] == 0);
