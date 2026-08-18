86122c4ef64b68e44c1689858567a7ac7fc281e8
KyoheiAbc <autumn8november@gmail.com>
2026-08-18 13:47:22 +0900

    Add size function and enhance assertions for group and cube properties

--- LEGO_Minifigure.js
+++ LEGO_Minifigure.js
@@ -45,3 +45,69 @@ rightArm.rotation[2] = 15
 leftArm.rotation[2] = -15
 
 Canvas.updateAll();
+
+
+function size(cube) {
+	return [cube.to[0] - cube.from[0], cube.to[1] - cube.from[1], cube.to[2] - cube.from[2]];
+}
+
+
+
+console.assert(Group.all[0].name == "RightLeg");
+console.assert(Group.all[0].origin[0] == 3);
+console.assert(Group.all[0].origin[1] == 9);
+console.assert(Group.all[0].children.length == 1);
+console.assert(Group.all[0].children[0].name == Group.all[0].name);
+
+console.assert(Group.all[1].name == "LeftLeg");
+console.assert(Group.all[1].origin[0] == -3);
+console.assert(Group.all[1].origin[1] == 9);
+console.assert(Group.all[1].children.length == 1);
+console.assert(Group.all[1].children[0].name == Group.all[1].name);
+
+console.assert(Group.all[2].name == "Body");
+console.assert(Group.all[2].origin[0] == 0);
+console.assert(Group.all[2].origin[1] == 18);
+console.assert(Group.all[2].children.length == 1);
+console.assert(Group.all[2].children[0].name == Group.all[2].name);
+
+console.assert(Group.all[3].name == "Head");
+console.assert(Group.all[3].origin[0] == 0);
+console.assert(Group.all[3].origin[1] == 29);
+console.assert(Group.all[3].children.length == 2);
+console.assert(Group.all[3].children[0].name == Group.all[3].name);
+console.assert(Group.all[3].children[1].name == "Neck");
+
+console.assert(Group.all[4].name == "RightArm");
+console.assert(Group.all[4].origin[0] == 6 + 1.5 - 1);
+console.assert(Group.all[4].origin[1] == 24 - 1.5 - 1);
+console.assert(Group.all[4].children.length == 2);
+console.assert(Group.all[4].children[0].name == Group.all[4].name);
+console.assert(Group.all[4].children[1].name == "RightHand");
+
+console.assert(Group.all[5].name == "RightHand");
+console.assert(Group.all[5].origin[0] == Group.all[4].origin[0]);
+console.assert(Group.all[5].origin[1] == 12 + 2 - 1);
+console.assert(Group.all[5].children.length == 1);
+console.assert(Group.all[5].children[0].name == Group.all[5].name);
+
+console.assert(Group.all[6].name == "LeftArm");
+console.assert(Group.all[6].origin[0] == -6 - 1.5 + 1);
+console.assert(Group.all[6].origin[1] == 24 - 1.5 - 1);
+console.assert(Group.all[6].children.length == 2);
+console.assert(Group.all[6].children[0].name == Group.all[6].name);
+console.assert(Group.all[6].children[1].name == "LeftHand");
+
+console.assert(Group.all[7].name == "LeftHand");
+console.assert(Group.all[7].origin[0] == Group.all[6].origin[0]);
+console.assert(Group.all[7].origin[1] == 12 + 2 - 1);
+console.assert(Group.all[7].children.length == 1);
+console.assert(Group.all[7].children[0].name == Group.all[7].name);
+
+Group.all.forEach(group => {
+	console.assert(group.origin[2] == 0)
+});
+
+Cube.all.forEach(cube => {
+	console.assert(cube.origin[2] == 0);
+});
